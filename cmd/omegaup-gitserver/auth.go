953834521a98ed58a7fa5b4a5388b1e80fa6b810
lhchavez <lhchavez@lhchavez.com>
2019-07-16 03:19:32 +0000

    Add a way of verifying Argon2id hashes

--- cmd/omegaup-gitserver/auth.go
+++ cmd/omegaup-gitserver/auth.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"context"
+	"crypto/subtle"
 	"encoding/base64"
+	stderrors "errors"
 	"fmt"
 	"github.com/inconshreveable/log15"
 	"github.com/o1egl/paseto"
 	"github.com/omegaup/githttp"
 	"github.com/omegaup/gitserver/request"
 	"github.com/pkg/errors"
+	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/ed25519"
 	"net/http"
 	"strings"
 	"time"
+)
+
+var (
+	// ErrInvalidHash is returned if the hash is not in the correct format.
+	ErrInvalidHash = stderrors.New("the encoded hash is not in the correct format")
+	// ErrIncompatibleVersion is returned if the version of the hash is not 19.
+	ErrIncompatibleVersion = stderrors.New("incompatible version of argon2")
 )
 
 const (
@@ -25,6 +35,41 @@ type omegaupAuthorization struct {
 	log         log15.Logger
 	publicKey   ed25519.PublicKey
 	secretToken string
+}
+
+func verifyArgon2idHash(password, encodedHash string) (bool, error) {
+	tokens := strings.Split(encodedHash, "$")
+	if len(tokens) != 6 || tokens[0] != "" || tokens[1] != "argon2id" {
+		return false, ErrInvalidHash
+	}
+
+	var version int
+	if _, err := fmt.Sscanf(tokens[2], "v=%d", &version); err != nil {
+		return false, errors.Wrap(err, "failed to parse version")
+	}
+	if version != argon2.Version {
+		return false, ErrIncompatibleVersion
+	}
+
+	var memory uint32
+	var iterations uint32
+	var parallelism uint8
+	if _, err := fmt.Sscanf(tokens[3], "m=%d,t=%d,p=%d", &memory, &iterations, &parallelism); err != nil {
+		return false, errors.Wrap(err, "failed to parse hash parameters")
+	}
+
+	salt, err := base64.RawStdEncoding.DecodeString(tokens[4])
+	if err != nil {
+		return false, errors.Wrap(err, "failed to decode salt")
+	}
+
+	hash, err := base64.RawStdEncoding.DecodeString(tokens[5])
+	if err != nil {
+		return false, errors.Wrap(err, "failed to decode hash")
+	}
+
+	otherHash := argon2.IDKey([]byte(password), salt, iterations, memory, parallelism, uint32(len(hash)))
+	return subtle.ConstantTimeCompare(hash, otherHash) == 1, nil
 }
 
 func (a *omegaupAuthorization) parseBearerToken(token string) (username, problem string, ok bool) {
--- cmd/omegaup-gitserver/auth_test.go
+++ cmd/omegaup-gitserver/auth_test.go
@@ -11,6 +11,19 @@ const (
 	expiredToken = "v2.public.eyJwcm9ibGVtIjoiYTA5NGY3MWI0ZmEzZmY4YmVhZmY0MDZiNTQ1YTU1NzgiLCJleHAiOiIyMDE5LTAxLTA0VDA1OjAzOjA0KzAwOjAwIiwiaXNzIjoib21lZ2FVcCBmcm9udGVuZCIsInN1YiI6IjBiOGJhM2FjN2M2MzVkM2U2OTg5MWU4ODAzYzc4YTJmIn0VXEbZ414QQ7qEY9MFa2gGgeJqc-T1Ff7fPM3ZQ_YQudq5I05offZxG819gmdmq3Z1_HeV7bjMRWL5JIkfkHIK"
 )
 
+func TestVerifyArgon2idHash(t *testing.T) {
+	ok, err := verifyArgon2idHash(
+		"omegaup",
+		"$argon2id$v=19$m=1024,t=2,p=1$7IYovf67Xtv0EAvZnKTIsQ$VgdbmwOXz9QtoM/tbx6pKyjbdrMEmotTDJE+NzRNyK0",
+	)
+	if err != nil {
+		t.Fatalf("failed to verify hash: %v", err)
+	}
+	if !ok {
+		t.Errorf("expected the hash to match, didn't")
+	}
+}
+
 func TestParseBearerAuth(t *testing.T) {
 	log := base.StderrLog()
 	keyBytes, err := base64.StdEncoding.DecodeString(DefaultConfig().Gitserver.PublicKey)
